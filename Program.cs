c01447584499497abcd4de2286536a12dae146ec
neSux0 <senxu98@gmail.com>
2026-07-13 15:28:53 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace A5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+    class Phone
+    {
+        public string os = "None";
+        public virtual void Call()
+        {
+            Console.WriteLine("This is a phone.");
+        }
+    }
+
+    class IPhone : Phone
+    {
+        public IPhone()
+        {
+            os = "IOS";
+        }
+        public override void Call()
+        {
+            Console.WriteLine("This is an Iphone.");
+        }
+    }
+
+    class Android: Phone
+    {
+        public Android()
+        {
+           os = "Android OS";
+        }
+        public override void Call()
+        { 
+            Console.WriteLine("This is an Android.");
+        }
+    }
+}
